cfb89a3bc04822cf5d1f323a8bc4d74a2edf47af
James Barrett <57573002+JamesBarrettMsc@users.noreply.github.com>
2025-03-05 17:58:26 +0000

    Add files via upload

--- sentiment analysis.py
+++ sentiment analysis.py
@@ -2,31 +2,36 @@
 sentence = input("what you massage : ")
 sentence = sentence.lower().split()
  
-positive_word= ["good","cool"]
-negative_word= ["bad","uncool"]
+positive_word= ["good","cool","bmazing", "brilliant"]
+negative_word= ["bad","uncool","awful", "lame"]
  
 positive_level=0
 negative_level=0
 
  
-mood = "Neutral"
+sentiment = "Neutral"
  
 for word in sentence:
     if word in positive_word:
         positive_level+=1
+        
     elif word in negative_word:
         negative_level+=1
  
 else:
-    if positive_level-negative_word <0:
-        mood = "negative"
+    if positive_level-negative_level <0:
+        sentiment = "Negative"
+        
+    elif(positive_level-negative_level >0):
+        sentiment = "Positive"
+        
+print("="*20)
 
 print(f"Total number of words  {len(sentence)}")
 print(f"Number of positive words {positive_level}")
 print(f"Number of positive negative {negative_level}")
-    
-    
-    
- 
-print(mood)
+
+print("="*20)
+print(f"The Sentiment Analysis {sentiment}")
+print("="*20)
  
